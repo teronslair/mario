fbf87eca00689168ad9fc24a1fedd636343cd772
teronslair <razvan.ionut.olteanu@gmail.com>
2016-02-23 17:52:26 +0200

    Added files via upload

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.IO
 import Graphics.UI.GLUT
+import Foreign.C.Types
 
 data GameElement = GameElement {
 						x :: GLfloat,
@@ -21,24 +22,44 @@ main = do
 	(_progName, _args) <- Graphics.UI.GLUT.getArgsAndInitialize
 	_window <- Graphics.UI.GLUT.createWindow "Hello World"
 	Graphics.UI.GLUT.displayCallback $= display
+	reshapeCallback $= Just reshape
 	mainLoop
+
+reshape :: ReshapeCallback
+reshape size = do
+	viewport $= (Position 0 0, size)
+	postRedisplay Nothing
 
 color3f r g b = color $ Color3 r g (b :: GLfloat)
 vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
 
-renderGame :: GameElement -> IO ()
-renderGame g = do {
-					color3f 1 0 0;
-					vertex3f (x g - (w g) / 2 ) (y g - (h g) / 2 ) 0;
-					vertex3f (x g + (w g) / 2 ) (y g - (h g) / 2 ) 0;
-					vertex3f (x g + (w g) / 2 ) (y g + (h g) / 2 ) 0;
-					vertex3f (x g - (w g) / 2 ) (y g + (h g) / 2 ) 0;
-					}
+my_translate :: GameElement -> GameElement
+my_translate g = g {x=(x g)-500, y=(y g)-500}
+-- my_translate g = g {x=x-(500 :: GLfloat), y=y-(500 :: GLfloat)}
+-- my_translate g = GameElement {x=(x g)-500, y=(y g)-500, vx = (vx g), vy = (vy g), w = (w g), h = (h g)}
+
+my_normalize :: GameElement -> GameElement
+-- my_normalize g = g {x = (x :: CFloat )/1000, y=y/1000, w=w/1000, h=h/1000}
+my_normalize g = g {x = (x g)/1000, y=(y g)/1000, w=(w g)/1000, h=(h g)/1000}
+-- my_normalize g = GameElement {x=(x g)/1000, y=(y g)/1000, vx = (vx g), vy = (vy g), w = (w g)/1000, h = (h g)/1000}
+
+
+renderGameElement :: GameElement -> IO ()
+renderGameElement myge = do {
+								let {g = my_normalize $ my_translate myge};
+								-- let g = GameElement {x=20, y=40, vx=0, vy=0, w=10, h=10};
+								-- let a = 3;
+								color3f 1 0 0;
+								vertex3f (x g - (w g) / 2 ) (y g - (h g) / 2 ) 0;
+								vertex3f (x g + (w g) / 2 ) (y g - (h g) / 2 ) 0;
+								vertex3f (x g + (w g) / 2 ) (y g + (h g) / 2 ) 0;
+								vertex3f (x g - (w g) / 2 ) (y g + (h g) / 2 ) 0;
+							}
  
 display :: DisplayCallback
 display = do
 	clear [ ColorBuffer ]
-	let myGame = [GameElement {x=0, y=0, vx=0, vy=0, w=1, h=1} , GameElement {x=1.0, y=1.0, vx=0, vy=0, w=1, h=1}]
+	let myGame = [GameElement {x=20, y=40, vx=0, vy=0, w=10, h=10} , GameElement {x=100, y=50, vx=0, vy=0, w=100, h=50}]
 	-- renderPrimitive Quads (do {
 	-- 							    color3f 1 0 0;
 	-- 							    vertex3f 0 0 0;
@@ -46,5 +67,5 @@ display = do
 	-- 							    vertex3f 0.2 0.2 0;
 	-- 							    vertex3f 0.2 0 0; 
 	-- 							})
-	foldl (\a x -> a >> x) (return ()) (map (renderPrimitive Quads) (map renderGame myGame))
+	foldl (\a x -> a >> x) (return ()) (map (renderPrimitive Quads) (map renderGameElement myGame))
 	flush
